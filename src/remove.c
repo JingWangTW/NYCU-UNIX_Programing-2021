baa6a5dd51e4a5dba8cf8fae8ed4e8f08f058aa4
Jing Wang <jingwang.cs08g@nctu.edu.tw>
2021-04-28 07:17:25 +0000

    Finish remove hijack

---
+++ src/remove.c
@@ -0,0 +1,28 @@
+#define _GNU_SOURCE
+
+#include <dlfcn.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#include "utility.h"
+
+int remove ( const char * pathname )
+{
+    static int ( *linux_remove ) ( const char * ) = NULL;
+    int ret_value;
+
+    FILE * output_file = get_output_file ( );
+
+    if ( linux_remove == NULL )
+    {
+        linux_remove = dlsym ( RTLD_NEXT, "remove" );
+    }
+
+    ret_value = linux_remove ( pathname );
+
+    fprintf ( output_file, "[logger] remove(%p) = %d\n", pathname, ret_value );
+
+    close_output_file ( output_file );
+
+    return ret_value;
+}
